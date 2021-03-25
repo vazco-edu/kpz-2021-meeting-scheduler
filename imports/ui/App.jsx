df4060a7a37c586b5794529b84f51273773e8202
Kamil Hulewicz <hulewicz.k@gmail.com>
2021-03-25 21:42:07 +0100

    Google AOuth scopes #1

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { Task } from './Task';
-import { GoogleLogin } from 'react-google-login';
-import { useGoogleLogout } from 'react-google-login'
-import { useGoogleLogin } from 'react-google-login'
 
 const tasks = [
     {_id: 1, text: 'First Task'},
     {_id: 2, text: 'Second Task'},
     {_id: 3, text: 'Third Task'},
 ];
-
-const responseGoogle = (response) => {
-    console.log(response);
-}
 
 export const App = () => (
     <div>
--- imports/ui/GoogleLoginForm.jsx
+++ imports/ui/GoogleLoginForm.jsx
@@ -2,19 +2,23 @@ import { GoogleLogin } from 'react-google-login';
 import React from "react";
 
 const responseGoogle = (response) => {
-    console.log(response)
+    console.log(response.profileObj)
 }
 
+const scopes = 'https://www.googleapis.com/auth/calendar.readonly '
 export const GoogleForm = () => (
     <div>
         <h5>Google Login</h5>
         <GoogleLogin
             //Here you shall enter the Google OAuth API Key ;) Will work on some env variables later
-            clientId="554233476862-r9gf09tip562md76upbuiv0mh6lro8mp.apps.googleusercontent.com"
-            buttonText="Login"
+            clientId=""
+            buttonText="Login with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
+            cookiePolicy={"single_host_origin"}
+            theme={"dark"}
+            scope="aa"
         />
     </div>
+
 );

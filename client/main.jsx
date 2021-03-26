7fa3227ddac67e40311df0a7eb82e3af36c096b5
Kamil Hulewicz <hulewicz.k@gmail.com>
2021-03-26 19:43:56 +0100

    Google Account scopes added #1

--- client/main.jsx
+++ client/main.jsx
@@ -4,7 +4,6 @@ import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
 import {GoogleForm} from "../imports/ui/GoogleLoginForm";
 
-
 Meteor.startup(() => {
   render(<App/>, document.getElementById('react-target'));
   render(<GoogleForm/>, document.getElementById('google-login-form'))
--- imports/ui/GoogleLoginForm.jsx
+++ imports/ui/GoogleLoginForm.jsx
@@ -5,7 +5,6 @@ const responseGoogle = (response) => {
     console.log(response.profileObj)
 }
 
-const scopes = 'https://www.googleapis.com/auth/calendar.readonly '
 export const GoogleForm = () => (
     <div>
         <h5>Google Login</h5>
@@ -16,8 +15,21 @@ export const GoogleForm = () => (
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
             cookiePolicy={"single_host_origin"}
-            theme={"dark"}
-            scope="aa"
+            scope={"https://www.googleapis.com/auth/userinfo.email" +
+            " https://www.googleapis.com/auth/userinfo.profile" +
+            " https://www.googleapis.com/auth/calendar.calendarlist.readonly" +
+            " https://www.googleapis.com/auth/calendar.app.created" +
+            " https://www.googleapis.com/auth/calendar.events.freebusy" +
+            " https://www.googleapis.com/auth/calendar.events.public.readonly" +
+            " https://www.googleapis.com/auth/calendar" +
+            " https://www.googleapis.com/auth/calendar.readonly" +
+            " https://www.googleapis.com/auth/calendar.events" +
+            " https://www.googleapis.com/auth/calendar.events.owned" +
+            " https://www.googleapis.com/auth/calendar.events.owned.readonly" +
+            " https://www.googleapis.com/auth/calendar.events.readonly" +
+            " https://www.googleapis.com/auth/calendar.calendarlist" +
+            " https://www.googleapis.com/auth/calendar.calendars" +
+            " https://www.googleapis.com/auth/calendar.calendars.readonly"}
         />
     </div>
 

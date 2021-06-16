01225694aa199b3707fe637f8acb5624449a8f6a
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-06-16 20:47:00 +0200

    Save response data

--- react_meeting_scheduler/src/components/CalendarForm.tsx
+++ react_meeting_scheduler/src/components/CalendarForm.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent} from "react";
 import CalendarCard from "./CalendarCard";
 import CalendarFromResponse from "./CalendarFromResponse";
 import axios from "axios";
+import {Redirect} from "react-router-dom";
 
 interface MyState {
     calendarsChecked: string[];
@@ -10,6 +11,7 @@ interface MyState {
     beginning_time: any;
     ending_time: any;
     duration: any;
+    isSubmitted: boolean;
 }
 interface IRequestData {
     calendars: string[];
@@ -23,6 +25,13 @@ interface IRequestData {
     "meeting_duration_minutes": string,
 }
 
+interface IResponseData {
+    calendars: string[];
+    dates: Date[];
+    duration_hours: number;
+    duration_minutes: number;
+}
+
 export default class CalendarForm extends React.Component<any , MyState> {
     calendarList: CalendarFromResponse[] ;
     constructor(props: any) {
@@ -33,7 +42,8 @@ export default class CalendarForm extends React.Component<any , MyState> {
             ending_date: null,
             beginning_time: null,
             ending_time: null,
-            duration: null
+            duration: null,
+            isSubmitted: false
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -56,10 +66,14 @@ export default class CalendarForm extends React.Component<any , MyState> {
     }
 
     handleSubmit(event: React.ChangeEvent<HTMLInputElement> | any) {
+        this.setState({
+            isSubmitted: true
+        })
+        console.log(this.state.isSubmitted)
         let [beginning_hours, beginning_minutes] = this.state.beginning_time.split(':').
-            map((element: string) => parseInt(element))
+        map((element: string) => parseInt(element))
         let [ending_hours, ending_minutes] = this.state.ending_time.split(':').
-            map((element: string) => parseInt(element))
+        map((element: string) => parseInt(element))
         let [meeting_duration_hours, meeting_duration_minutes] = this.state.duration.split(':').
         map((element: string) => parseInt(element))
 
@@ -75,33 +89,43 @@ export default class CalendarForm extends React.Component<any , MyState> {
         body.meeting_duration_minutes = meeting_duration_minutes
 
         axios.post(
-            "http://zajonc.com",
+            "http://localhost:8000/api/calendars/algorithm",
             {
                 body
             }
         ).then(response => {
+            let responseData = response as unknown as IResponseData
+            localStorage.setItem("dates", JSON.stringify(responseData.dates));
+            localStorage.setItem("calendars", JSON.stringify(responseData.calendars));
+            localStorage.setItem("duration_hours", JSON.stringify(responseData.duration_hours));
+            localStorage.setItem("duration_minutes", JSON.stringify(responseData.duration_minutes));
+            localStorage.setItem("duration_minutes", JSON.stringify(responseData.duration_minutes));
         })
     }
 
 
 
     render() {
+        if(this.state.isSubmitted){
+            return <Redirect to='events'/>
+        }
+
         this.calendarList = JSON.parse(localStorage.getItem("calendar")) as CalendarFromResponse[];
         return (
             <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
                 {this.calendarList != null ? this.calendarList.map(calendar => <CalendarCard id={calendar.id} summary={calendar.summary}
                                                                                              description={calendar.description}/>) : console.log("Empty calendar list!")}
-                    <label htmlFor="beginning_date"> Beginning date: </label>
-                    <input type="date" id="beginning_date" value={this.state.beginning_date}/>
-                    <label htmlFor="ending_date"> Ending date: </label>
-                    <input type="date" id="ending_date" value={this.state.ending_date}/>
-                    <label htmlFor="beginning_time"> Beginning hour and minute: </label>
-                    <input type="time" id="beginning_time" value={this.state.beginning_time}/>
-                    <label htmlFor="ending_time"> Ending hour and minute: </label>
-                    <input type="time" id="ending_time" value={this.state.ending_time}/>
-                    <label htmlFor="duration"> Meeting duration time: </label>
-                    <input type="time" id="duration" value={this.state.duration}/>
-                    <input type="submit" value="Send"/>
+                <label htmlFor="beginning_date"> Beginning date: </label>
+                <input type="date" id="beginning_date" value={this.state.beginning_date} required={true}/>
+                <label htmlFor="ending_date"> Ending date: </label>
+                <input type="date" id="ending_date" value={this.state.ending_date} required={true}/>
+                <label htmlFor="beginning_time"> Beginning hour and minute: </label>
+                <input type="time" id="beginning_time" value={this.state.beginning_time} required={true}/>
+                <label htmlFor="ending_time"> Ending hour and minute: </label>
+                <input type="time" id="ending_time" value={this.state.ending_time} required={true}/>
+                <label htmlFor="duration"> Meeting duration time: </label>
+                <input type="time" id="duration" value={this.state.duration} required={true}/>
+                <input type="submit" value="Send"/>
 
             </form>
         );
